b7f9afcfb1deecf1cd6837ef04510771b07b611d
Leeyaoi <darya.ya2608@gmail.com>
2024-05-30 11:36:43 +0300

    fix: fixed access to cart

--- script/cart.js
+++ script/cart.js
@@ -1,57 +1,63 @@
-if(cartArray.length == 0){
-    document.querySelector(".cart_header").innerHTML = "Your cart is empty" 
-    document.querySelector(".cart_header").dataset.lang = "empty"
-    document.querySelector(".total_cost").innerHTML += "0.00" 
-}
-else{
-    document.querySelector(".cart_header").innerHTML += "("+cartArray.length+")" 
-    const wrapper = document.querySelector(".wrapper")
-    let price = 0.0
-    for(let i = 0; i < cartArray.length; i++){
-        price += parseFloat(cartArray[i]["price"])
-        wrapper.appendChild(createElement(cartArray[i], i))
-        document.querySelector(".del_btn.b"+i).addEventListener("click", () => {
-            deleteProduct(cartArray[i]["image"])
-            location.reload()
-        })
-    }
-    document.querySelector(".total_cost").innerHTML += price 
+if (
+  localStorage.getItem("role") != "seller" &&
+  localStorage.getItem("role") != "client"
+) {
+  window.location.replace("../pages/log_in_page.html");
 }
 
-function createElement(product, i){
-    const newDiv = document.createElement("div");
-    newDiv.classList.add("prod")
-    newDiv.classList.add("b"+i)
+if (cartArray.length == 0) {
+  document.querySelector(".cart_header").innerHTML = "Your cart is empty";
+  document.querySelector(".cart_header").dataset.lang = "empty";
+  document.querySelector(".total_cost").innerHTML += "0.00";
+} else {
+  document.querySelector(".cart_header").innerHTML +=
+    "(" + cartArray.length + ")";
+  const wrapper = document.querySelector(".wrapper");
+  let price = 0.0;
+  for (let i = 0; i < cartArray.length; i++) {
+    price += parseFloat(cartArray[i]["price"]);
+    wrapper.appendChild(createElement(cartArray[i], i));
+    document.querySelector(".del_btn.b" + i).addEventListener("click", () => {
+      deleteProduct(cartArray[i]["image"]);
+      location.reload();
+    });
+  }
+  document.querySelector(".total_cost").innerHTML += price;
+}
 
-    const image = document.createElement("img");
-    image.src = "../source/products/prod"+product["image"]+"/1.png"
-    image.classList.add("prod_img")
+function createElement(product, i) {
+  const newDiv = document.createElement("div");
+  newDiv.classList.add("prod");
+  newDiv.classList.add("b" + i);
 
-    const name = document.createElement("p")
-    name.innerHTML = product["name"]
-    name.classList.add("prod_name")
-    name.dataset.lang = "b"+product["image"]
+  const image = document.createElement("img");
+  image.src = "../source/products/prod" + product["image"] + "/1.png";
+  image.classList.add("prod_img");
 
-    const price = document.createElement("p")
-    price.innerHTML = "$"+product["price"]
-    price.classList.add("prod_price")
+  const name = document.createElement("p");
+  name.innerHTML = product["name"];
+  name.classList.add("prod_name");
+  name.dataset.lang = "b" + product["image"];
 
-    const btn = document.createElement("div")
-    if(localStorage.getItem("lang") == "ru"){
-        btn.innerHTML = "Удалить"
-    }
-    else{
-        btn.innerHTML = "delete"
-    }
-    btn.classList.add("button")
-    btn.classList.add("del_btn")
-    btn.classList.add("b"+i)
-    btn.dataset.lang = "del_btn"
+  const price = document.createElement("p");
+  price.innerHTML = "$" + product["price"];
+  price.classList.add("prod_price");
 
-    newDiv.appendChild(image)
-    newDiv.appendChild(name)
-    newDiv.appendChild(price)
-    newDiv.appendChild(btn)
+  const btn = document.createElement("div");
+  if (localStorage.getItem("lang") == "ru") {
+    btn.innerHTML = "Удалить";
+  } else {
+    btn.innerHTML = "delete";
+  }
+  btn.classList.add("button");
+  btn.classList.add("del_btn");
+  btn.classList.add("b" + i);
+  btn.dataset.lang = "del_btn";
 
-    return newDiv
+  newDiv.appendChild(image);
+  newDiv.appendChild(name);
+  newDiv.appendChild(price);
+  newDiv.appendChild(btn);
+
+  return newDiv;
 }
